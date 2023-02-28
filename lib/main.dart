b0d4feb78b80106b9f15540d52784fd23a0a0d19
Aditya <adityabhatt78910@gmail.com>
2023-02-28 23:07:32 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
+import 'package:cataloge/homepage.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
-    const MyApp({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context){
-      return MaterialApp(
-        home: Material(
-          child: Center(
-            child: Container(
-              child: Text("Welcome"),
-            ),
-          ),
-        ),
-      );
-    }
+class MyApp extends StatelessWidget {
+  int day = 2;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
